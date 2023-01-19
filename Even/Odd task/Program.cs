639c6c0a28adc2875be4471b11dd3d40f0b6b75d
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-19 22:14:07 +0700

    Even/Odd task Folder Solved

--- Even/Odd task/Program.cs
+++ Even/Odd task/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (num %2 == 0)
+    {
+        Console.Write("Число четное");
+    }
+else 
+    {
+        Console.Write("Число нечетное");
+    }    
