6906032a2ec08b84531135e4c4476d40fb5f8e3b
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-19 22:57:18 +0700

    Last number of three-digit number task solved

--- Last number of three-digit number task/Program.cs
+++ Last number of three-digit number task/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int y = num %10;
+Console.Write("Последняя цифра трехзначного числа: " + y);
+
+//Решение с рандомайзером:
+//int num = new Random().Next(100, 1000);
+//int y = num%10;
+//Console.Write("Трехзначное число: " + num + " Последняя цифра трезначного числа: " + y);
