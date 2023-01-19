32cc8f7504985967de52617e23e0272cda783e7e
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-19 22:35:23 +0700

    Even numbers from 1 to N task Solved

--- Even numbers from 1 to N/Program.cs
+++ Even numbers from 1 to N/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите последнее число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+for (i = 1; i <= N; i++)
+    if (i %2 == 0)
+        {
+            Console.WriteLine(i);
+        }
