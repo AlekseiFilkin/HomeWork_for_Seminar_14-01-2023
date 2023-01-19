6c2f7da7ee611a370cee7758d10096ab52976211
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-19 21:43:57 +0700

    Square Task solved

--- Square task/Program.cs
+++ Square task/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Проверка, не является ли число 1 квадратом 2ого числа
+Console.Write("Введите первое целое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе целое число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+if (num1 == num2 * num2)
+    {Console.Write("Первое число " + num1 + " является квадратом второго числа " + num2);
+
+    }
+else 
+    {Console.Write("Первое число " + num1 + " не является квадратом второго числа " + num2);
+
+}
