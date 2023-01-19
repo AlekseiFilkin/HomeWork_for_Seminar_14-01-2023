6705b1f06b6512ec7f0a3ce2e3bddca288b0aa63
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-20 01:33:15 +0700

    Max number range task solved

--- Max number range task/Program.cs
+++ Max number range task/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Загаданное число: {num}");
+//Console.Write("Введите число от 10 до 99: ");
+//int num = int.Parse(Console.ReadLine() ?? "0");
+int max = num %10;
+while ( num != 0 )
+    {
+        num = num /= 10;
+        
+        if (num%10 > max)
+            max = num %10;
+    }
+Console.WriteLine($"Макcимальная цифра числа равна {max}");
