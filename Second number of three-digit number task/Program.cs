331d2feef9d9ddbdd394efa6eb1e6a9fef0f1ff0
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-20 00:27:48 +0700

    Second number of three-digit number task solved

--- Second number of three-digit number task/Program.cs
+++ Second number of three-digit number task/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+while (num >= 100)
+{
+    num /= 10;
+}
+int res = num %10;
+Console.Write("Вторая цифра трехзначного числа: " + res);
