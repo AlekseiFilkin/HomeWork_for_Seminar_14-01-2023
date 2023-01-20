bbfda6563a51161b1ff07f443173a4734a672aa6
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-20 22:20:17 +0700

    Multiplicity task solved

--- Multiplicity task/Program.cs
+++ Multiplicity task/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+if (num1 % num2 == 0)
+    {
+        Console.Write("Первое число кратно второму");
+    }
+    else
+    {
+        int res = num1 % num2;
+        Console.Write("Первое число не кратно второму, остаток от деления: " + res);
+    }
