9bde56183a1e1ace7c2092dc6b0d722317e7bb34
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-21 00:02:36 +0700

    Third digit of number task solved

--- Third digit of number task/Program.cs
+++ Third digit of number task/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (num / 100 > 0)
+    {
+        while (num > 1000)
+
+        num = num / 10; 
+        num = num % 10;
+        Console.Write("Третья цифра числа: " + num);
+    }
+        
+    else
+    { 
+        Console.Write("Третьей цифры нет");       
+    }
+            
