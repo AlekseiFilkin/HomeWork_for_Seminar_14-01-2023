62bcb0cbf7f6e02d0369c82c7609fd61c1c569d8
Aleksey_Filkin <acid_wind@mail.ru>
2023-01-20 21:20:49 +0700

    Delete second digit of three-digit number task solved

---
+++ Delete second digit of three-digit number task/Program.cs
@@ -0,0 +1,5 @@
+﻿//Удалить вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int res = (num % 10) + (num / 100) * 10;
+Console.WriteLine($"Удалив вторую цифру получится: {res}");
